ad46afaadaa7fe1d20952f05e4efe6b47f969679
lopuv <400917@vistacollege.nl>
2022-11-04 11:22:09 +0100

    first buttons

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -1,24 +1,17 @@
 ﻿namespace Lindssen_app;
 
+using System.Diagnostics;
+
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
 	public MainPage()
 	{
 		InitializeComponent();
 	}
 
-	private void OnCounterClicked(object sender, EventArgs e)
+	private void Button1_Clicked(object sender, EventArgs e)
 	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
+		Debug.WriteLine("the call comes from button");
 	}
 }
 
