05a2596988f481fd3672d7be1ed88b267c05a118
lopuv <400917@vistacollege.nl>
2022-11-08 13:13:47 +0100

    Button colors

--- info.xaml.cs
+++ info.xaml.cs
@@ -1,9 +1,32 @@
+using Microsoft.VisualBasic;
+
 namespace Lindssen_app;
 
 public partial class info : ContentPage
 {
+	int choice;
 	public info()
 	{
 		InitializeComponent();
+	}
+
+	private void Info_button_Clicked(object sender, EventArgs e)
+	{
+		if (choice == 0)
+		{
+            info_button.BackgroundColor = Colors.Red;
+			choice++;
+        }
+		else if(choice == 1)
+		{
+			info_button.BackgroundColor = Colors.Blue;
+			choice++;
+		}
+		else
+		{
+			info_button.BackgroundColor = Colors.Coral;
+			choice = 0;
+		}	
+
 	}
 }
