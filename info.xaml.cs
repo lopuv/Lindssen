8d60fbffda8eac1ccef67c776382d45ef6103e85
lopuv <400917@vistacollege.nl>
2022-11-14 21:17:43 +0100

    json connection

--- info.xaml.cs
+++ info.xaml.cs
@@ -1,11 +1,40 @@
+using Microsoft.Maui.Controls;
 using Microsoft.VisualBasic;
+using Newtonsoft.Json;
+using System.Configuration;
 
-namespace Lindssen_app;
-
-public partial class info : ContentPage
+namespace Lindssen_app
 {
-	public info()
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class info : ContentPage
 	{
-		InitializeComponent();
+		public info()
+		{
+			InitializeComponent();
+
+		}
+
+        private void citroen_Clicked(object sender, EventArgs e)
+        {
+			Getinfo();
+        }
+
+		public async void Getinfo()
+		{
+			HttpClient client = new HttpClient();
+
+			var response = await client.GetStringAsync("http://192.168.178.143/yachts/data.json");
+			User useR = JsonConvert.DeserializeObject<User>(response);
+			banaan.Text = useR.Id;
+		}
+
+      
+    }
+
+    public class User
+	{
+		public string Id { get; set; }
+		public string Username { get; set; }
+		public string Password { get; set; }
 	}
 }
